b76018ac2dc9bfe418065431c8a3c883506c9cb9
Dandy Candra <dandycandrapratama@gmail.com>
2025-02-04 13:53:49 +0700

    add freelance link

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,37 +1,59 @@
+import { Sribu, Fastwork } from '~/components/partials/SVG';
+
 const Footer = () => {
   return (
-    <div className='mt-40 w-[400px] phone:mb-10 tablet:mb-10'>
-      <p className='text-sm font-light text-customSlate'>
-        Built and coded with{' '}
+    <div className='mt-40 w-[400px] phone:mb-10 phone:w-full tablet:mb-10'>
+      <div className='mb-3 flex flex-row items-center gap-5'>
         <a
-          href='https://remix.run/'
-          className='font-semibold text-customWhite hover:text-customGreen'
+          href='https://www.sribu.com/id/users/dandycandra'
+          target='_blank'
+          rel='noreferrer'
         >
-          Remix
-        </a>{' '}
-        and{' '}
-        <a
-          href='https://tailwindcss.com/'
-          className='font-semibold text-customWhite hover:text-customGreen'
-        >
-          Tailwind CSS
+          <Sribu />
         </a>
-        , deployed with{' '}
+
         <a
-          href='https://vercel.com/'
-          className='font-semibold text-customWhite hover:text-customGreen'
+          href='https://fastwork.id/user/dandycp'
+          target='_blank'
+          rel='noreferrer'
         >
-          Vercel
+          <Fastwork />
         </a>
-        . All text is set in the{' '}
-        <a
-          href='https://fonts.google.com/specimen/Open+Sans'
-          className='font-semibold text-customWhite hover:text-customGreen'
-        >
-          Open Sans
-        </a>
-        .
-      </p>
+      </div>
+
+      <div className='w-full'>
+        <p className='text-sm font-light text-customSlate'>
+          Built and coded with{' '}
+          <a
+            href='https://remix.run/'
+            className='font-semibold text-customWhite hover:text-customGreen'
+          >
+            Remix
+          </a>{' '}
+          and{' '}
+          <a
+            href='https://tailwindcss.com/'
+            className='font-semibold text-customWhite hover:text-customGreen'
+          >
+            Tailwind CSS
+          </a>
+          , deployed with{' '}
+          <a
+            href='https://vercel.com/'
+            className='font-semibold text-customWhite hover:text-customGreen'
+          >
+            Vercel
+          </a>
+          . All text is set in the{' '}
+          <a
+            href='https://fonts.google.com/specimen/Open+Sans'
+            className='font-semibold text-customWhite hover:text-customGreen'
+          >
+            Open Sans
+          </a>
+          .
+        </p>
+      </div>
     </div>
   );
 };
