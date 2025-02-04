7398668b59f3835c9fe3ed92be942a32a71e626f
Dandy Candra <dandycandrapratama@gmail.com>
2025-02-04 20:14:32 +0700

    add new SVG components

--- app/components/partials/SVG/index.ts
+++ app/components/partials/SVG/index.ts
@@ -3,3 +3,5 @@ export { default as Linkedin } from './Linkedin';
 export { default as Mail } from './Mail';
 export { default as Sribu } from './Sribu';
 export { default as Fastwork } from './Fastwork';
+export { default as Chain } from './Chain';
+export { default as ArrowLink } from './ArrowLink';
