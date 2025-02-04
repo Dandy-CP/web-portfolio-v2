5b4c67a3b5cbd422fced3919e8b6ed3d66d976c8
Dandy Candra <dandycandrapratama@gmail.com>
2025-02-04 13:55:21 +0700

    change meta name and content and render cursor gradient

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,27 +1,43 @@
+import { useRef } from 'react';
 import type { MetaFunction } from '@remix-run/node';
 import { Footer, SideContent } from '~/components/layout';
 import { About, Experience, Projects } from '~/components/modules/home';
+import { CursorGradient } from '~/components/partials';
+import { useMousePosition } from '~/hooks';
 
 export const meta: MetaFunction = () => {
   return [
     { title: 'Dandy Candra' },
-    { name: 'Personal Website', content: 'Personal Website Portfolio' },
+    {
+      name: 'Dandy Candra Personal Website',
+      content:
+        'Dandy Candra Frontend Engineer - I build things for the web and mobile, producing scalable, modular and easy to manage applications',
+    },
   ];
 };
 
 export default function Index() {
+  const divRef = useRef<HTMLDivElement>(null);
+  const mousePosition = useMousePosition({ ref: divRef });
+
   return (
-    <div className='flex h-full w-full px-40 phone:flex-col phone:px-3 tablet:flex-col tablet:px-10 desktop:px-10'>
-      <div className='sticky top-0 max-h-screen w-2/5 py-24 phone:relative phone:w-full phone:py-10 tablet:relative tablet:w-full tablet:py-10'>
-        <SideContent />
-      </div>
+    <div className='relative' ref={divRef}>
+      <CursorGradient
+        mousePositionX={mousePosition.x}
+        mousePositionY={mousePosition.y}
+      />
 
-      <div className='w-7/12 py-24 phone:w-full phone:py-0 tablet:w-full tablet:py-0'>
-        <About />
-        <Experience />
-        <Projects />
+      <div className='flex min-h-screen w-full px-40 phone:flex-col phone:px-3 tablet:flex-col tablet:px-10 desktop:px-10'>
+        <div className='sticky top-0 max-h-screen w-2/5 py-24 phone:relative phone:w-full phone:py-10 tablet:relative tablet:w-full tablet:py-10'>
+          <SideContent />
+        </div>
 
-        <Footer />
+        <div className='w-7/12 py-24 phone:w-full phone:py-0 tablet:w-full tablet:py-0'>
+          <About />
+          <Experience />
+          <Projects />
+          <Footer />
+        </div>
       </div>
     </div>
   );
