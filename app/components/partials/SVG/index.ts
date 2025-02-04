7f305d1a44796c3d067888db84814fa9990ef9c0
Dandy Candra <dandycandrapratama@gmail.com>
2025-02-04 13:54:45 +0700

    add new SVG

--- app/components/partials/SVG/index.ts
+++ app/components/partials/SVG/index.ts
@@ -1,3 +1,5 @@
 export { default as Github } from './Github';
 export { default as Linkedin } from './Linkedin';
 export { default as Mail } from './Mail';
+export { default as Sribu } from './Sribu';
+export { default as Fastwork } from './Fastwork';
